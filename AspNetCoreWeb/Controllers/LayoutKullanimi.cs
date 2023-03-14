a66bfb4ce6b3f6729d279ff56b32be05e67d11c9
OnurSen <onur.sen@email.com>
2023-03-14 19:08:05 +0300

    Add project files.

---
+++ AspNetCoreWeb/Controllers/LayoutKullanimi.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWeb.Controllers
+{
+    public class LayoutKullanimi : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult layoutyok()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetCoreWeb/Controllers/OrnekController1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWeb.Controllers
+{
+    public class urunler
+    {
+        public int Id { get; set; }
+        public String UrunAdı { get; set; }
+
+    }
+
+
+
+    public class OrnekController1 : Controller
+    {
+        public IActionResult Index()
+        {
+
+            TempData["İller"] = "Adana";
+          
+
+
+            ViewBag.kisiler = new { Id = 1, İsim = "Murat", Yaş = 42 };
+           
+            ViewData["Yaş"] = 30;
+
+
+            ViewData["İsimler"] = new List<string>() { "Ali", "Mehmet", "Furkan", "Deniz" };
+          
+            ViewBag.isim = "Onur.Sen";
+            return View();
+
+
+
+        }
+
+
+
+        public IActionResult index2()
+        {
+
+            return RedirectToAction("index", "ornekcontroller1");
+
+        }
+
+
+        public IActionResult index3()
+        {
+
+            var urunlistesi = new List<urunler>()
+            {
+                new() {Id=1, UrunAdı="Teker"},
+                new() {Id=5, UrunAdı="Jant"},
+                new() {Id=12, UrunAdı="Cam Filmi"},
+
+            };
+
+            return View(urunlistesi);
+        }
+
+
+
+
+        public IActionResult ContentResult()
+        {
+            return Content("Denemedir.");
+        }
+
+
+
+
+    }
+
+}
