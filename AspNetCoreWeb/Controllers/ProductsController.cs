583a47f2899235f65575a7448fa7d32b74e2c2d5
OnurSen <onur.sen@email.com>
2023-03-20 16:42:59 +0300

    sql server ile bağlantı yapılıyor.

---
+++ AspNetCoreWeb/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using AspNetCoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWeb.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        private readonly ProductRepository _productRepository;
+
+        public ProductsController()
+        {
+
+            _productRepository = new ProductRepository();
+
+
+        }
+        public IActionResult Index()
+        {
+            var products = _productRepository.GetAll();
+            return View(products);
+        }
+        public IActionResult remove(int id)
+        {
+            _productRepository.remove(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult add()
+        {
+            return View();
+        }
+
+        public IActionResult update(int id)
+        {
+            return View();
+        }
+
+
+    }
+}
--- AspNetCoreWeb/Models/ProductRepository.cs
+++ AspNetCoreWeb/Models/ProductRepository.cs
@@ -2,5 +2,46 @@
 {
     public class ProductRepository
     {
+        private static List<product> _products = new List<product>()
+        {
+           new() { Id = 1, Name = "Kalem 1", Price = 100, Stock = 200 },
+           new () { Id = 2, Name = "Kalem 2", Price = 200, Stock = 300 },
+           new () { Id = 3, Name = "Kalem 3", Price = 300, Stock = 400 }
+        };
+
+        public List<product> GetAll() => _products;
+
+        public void add(product newproduct) => _products.Add(newproduct);
+
+        public void remove(int id)
+        {
+            var hasProduct = _products.FirstOrDefault(x => x.Id == id);
+
+            if (hasProduct == null)
+            {
+                throw new Exception($"Bu id({id})' ye sahip ürün bulunamadı.");
+            }
+            _products.Remove(hasProduct);
+        }
+
+        public void update(product updateproduct)
+        {
+
+            var hasproduct = _products.FirstOrDefault(x => x.Id == updateproduct.Id);
+
+            if (hasproduct == null)
+            {
+                throw new Exception($"Bu id({updateproduct.Id})'ye sahip ürün bulunmamaktadır.");
+            }
+            hasproduct.Name = updateproduct.Name;
+            hasproduct.Price = updateproduct.Price;
+            hasproduct.Stock = updateproduct.Stock;
+
+            var index = _products.FindIndex(x => x.Id == updateproduct.Id);
+
+            _products[index] = hasproduct;
+
+        }
+
     }
 }
